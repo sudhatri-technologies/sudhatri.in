1842b881f4b7d8ca439883974519ae1804f2731b
sudhatri-technologies <113518285+srisudhatri@users.noreply.github.com>
2023-03-30 16:52:13 +0530

    today update

--- srisudhatri/settings.py
+++ srisudhatri/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*!xdt63o78gqwghhtvok@d_f@ek)us0wfr5$8&8ob*ra-*_3r-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
